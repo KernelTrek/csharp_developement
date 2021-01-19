ce3d177e7fe0257a40ec394458d105683a607f3b
Macbook <jaehylee@Macbookui-MacBookPro.local>
2021-01-19 17:43:25 +0900

    2.Sytex clear

--- csharp_fundamental_scott/gradebook/src/GradeBook/Program.cs
+++ csharp_fundamental_scott/gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,34 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var x = 34.1;
+            var y = 10.3;
+            var result = x + y;
+            var numbers = new double[3];
+            numbers[0] = 12.7;
+            numbers[1] = 10.3;
+            numbers[2] = 6.11;
+
+            // var result2 = numbers[0];
+            // result2 = result2 + numbers[1];
+            // result2 = result2 + numbers[2];
+
+            var result2 = 0.0;
+            foreach (double number in numbers)
+            {
+                result2 += number;
+            }
+            System.Console.WriteLine(result2);
+
+            List<double> grades = new List<double>();
+            grades.Add(10.9);
+            grades.Add(19.9);
+            System.Console.WriteLine($"{grades[0]:N1}");
+            
+
+
+            System.Console.WriteLine($"{result}");
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]} !");
